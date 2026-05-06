9a5da9de39b84e70452ea0e7fde8321bc98dfde5
elmiya0711 <elmiya0711@gmail.com>
2026-05-06 21:09:24 +0700

    MY_ELANG.py
    Fix banner function to correctly print the ASCII art.

--- MY_ELANG.py
+++ MY_ELANG.py
@@ -8,8 +8,7 @@ from colorama import Fore, init
 init(autoreset=True)
 
 def banner():
- print(Fore.MAGENTA + r'''''
- 
+ print(Fore.MAGENTA + r''''
 ,---.    ,---.   ____     __             .-''-.    .---.        ____    ,---.   .--.  .-_'''-.    
 |    \  /    |   \   \   /  /          .'_ _   \   | ,_|      .'  __ `. |    \  |  | '_( )_   \   
 |  ,  \/  ,  |    \  _. /  '          / ( ` )   ',-./  )     /   '  \  \|  ,  \ |  ||(_ o _)|  '  
